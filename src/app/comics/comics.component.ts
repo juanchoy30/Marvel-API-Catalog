5c6c6e26573d5e2059551a79ff39f24332693f67
Juan Miguel Choy <juanchoy30@gmail.com>
2020-08-20 16:30:02 -0300

    carousel basics

--- src/app/comics/comics.component.ts
+++ src/app/comics/comics.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, ViewEncapsulation  } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, HostListener, Input} from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { switchMap } from 'rxjs/operators';
 import { CharacterService } from '../services/character.service';
+
+import { IssuesService } from '../services/issues.service';
 
 @Component({
   selector: 'app-comics',
@@ -19,8 +21,13 @@ export class ComicsComponent implements OnInit {
   selectedComic: any;
   newDate: string;
   errMsg: string;
+  class: string;                // Style of carousel
+  @Input() shownNumber: number; // To size the carousel
+  @Input() forNumber: number;   // To size the carousel
 
-  constructor(private characterService: CharacterService,
+  constructor(
+    private characterService: CharacterService,
+    private issuesService: IssuesService,
     private route: ActivatedRoute,
     private modalService: NgbModal) { }
 
@@ -31,9 +38,10 @@ export class ComicsComponent implements OnInit {
 
   getComics() {
     this.route.params.pipe(switchMap((params: Params) => {
-      return this.characterService.getComicCharacter(params['id'])}))
+      return this.issuesService.getComicCharacter(params['id'])}))
         .subscribe( comics => {
           this.comics = comics;
+          this.changeSize(window.screen.width); // This function sizes the carousel
         },
         errmess => {
           this.errMsg = <any>errmess;
@@ -55,5 +63,59 @@ export class ComicsComponent implements OnInit {
     this.modalService.open(longContent, { scrollable: true });
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.changeSize(event.target.innerWidth);
+  }
+
+
+  changeSize(arg: any) {
+    if (arg <= 375) { 
+      this.class = 'card-container col-12';
+      this.shownNumber = 1;
+      this.forNumber = 0;
+    } else if (arg <= 425) {
+      this.class = 'card-container col-12';
+      this.shownNumber = 1;
+      this.forNumber = 0;
+    } else if (arg <= 768) {
+
+      if ( this.comics.count <= 3 ) {
+        this.class = 'card-container col-6';
+        this.shownNumber = this.comics.count;
+        this.forNumber = this.comics.count-1;
+      } else {
+        this.class = 'card-container col-6';
+        this.shownNumber = 2;
+        this.forNumber = 1;
+      }
+
+    } else if (arg <= 1024) {
+
+      if ( this.comics.count <= 3 ) {
+        this.class = 'card-container col-6';
+        this.shownNumber = this.comics.count;
+        this.forNumber = this.comics.count-1;
+      } else {
+        this.class = 'card-container col-4';
+        this.shownNumber = 3;
+        this.forNumber = 2;
+      }
+
+    } else if (arg <= 1440) {
+
+      if ( this.comics.count <= 3 ) {
+        this.class = 'card-container col-4';
+        this.shownNumber = this.comics.count;
+        this.forNumber = this.comics.count-1;
+      } else {
+        this.class = 'card-container col-3';
+        this.shownNumber = 4;
+        this.forNumber = 3;
+      }
+
+    }
+  }
+  
 
 }
---
+++ src/app/services/issues.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { baseURL } from '../shared/baseUrl';
+import { publicKey, ts, hash } from '../shared/validationKeys';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IssuesService {
+
+  constructor(
+    private http: HttpClient,
+    private processHttpMsgService: ProcessHTTPMsgService) { }
+
+
+  getComicCharacter(id: number): Observable<any> {
+    let comicsURL = `${baseURL}/${id}/comics?apikey=${publicKey}&ts=${ts}&hash=${hash}`;
+    return this.http.get<any>(comicsURL)
+      .pipe(map((data:any) => data.data),
+        catchError(this.processHttpMsgService.handleError));
+  }
+  // The data is not being formated so the number of items of the JSON file is available
+  
+  getEventsCharacter(id: number): Observable<any> {
+    let comicsURL = `${baseURL}/${id}/events?apikey=${publicKey}&ts=${ts}&hash=${hash}`;
+    return this.http.get<any>(comicsURL)
+      .pipe(map((data:any) => data.data),
+        catchError(this.processHttpMsgService.handleError));
+  }
+}
